97ee5eb245d0c24a9c47254123cbe0ea560e6366
fil7 <filonova7@yandex.ru>
2016-12-20 14:06:43 +0700

    hw1: Добавить поле id в Meal/ MealWithExceed и реализовать генерацию счетчика

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -3,12 +3,16 @@ package ru.javawebinar.topjava.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * GKislin
  * 11.01.2015.
  */
 public class Meal {
+    private static AtomicLong counter = new AtomicLong(0);
+    private final long id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -16,6 +20,7 @@ public class Meal {
     private final int calories;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
+        this.id = counter.incrementAndGet();
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
--- src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/MealWithExceed.java
@@ -1,12 +1,17 @@
 package ru.javawebinar.topjava.model;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * GKislin
  * 11.01.2015.
  */
 public class MealWithExceed {
+
+    private static AtomicLong counter = new AtomicLong(0);
+    private final long id;
+
     private final LocalDateTime dateTime;
 
     private final String description;
@@ -16,6 +21,7 @@ public class MealWithExceed {
     private final boolean exceed;
 
     public MealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
+        this.id = counter.incrementAndGet();
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
